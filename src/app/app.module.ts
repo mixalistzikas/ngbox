90bf72a5a31e4857d3ccbbd1fb55b1a6baacd2b3
Michalis <mixalistzikas@gmail.com>
2017-04-12 09:04:57 +0300

    Fix documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgBoxModule } from './ngbox/ngbox.module';
-import { NgBoxService } from './ngbox/ngbox.service';
+import { NgBoxModule } from 'ngbox/ngbox.module';
+import { NgBoxService } from 'ngbox/ngbox.service';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 
